b56591a55ede36768a296665ef6452adcb212c06
Lakhan Rathi <KTPL-0116@kpoint.co.in>
2022-03-15 19:03:30 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,423 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  TextEditingController urgent = TextEditingController();
+  TextEditingController important = TextEditingController();
+  TextEditingController plan = TextEditingController();
+  TextEditingController hold = TextEditingController();
+  late String urgentText,importantText,planText,holdText;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if(urgent.text==null || urgent.text=="")
+        urgentText = "Urgent";
+      else
+        urgentText = urgent.text;
+
+      if(important.text==null || important.text=="")
+        importantText = "Important";
+      else
+        importantText = important.text;
+
+      if(plan.text==null || plan.text=="")
+        planText = "Need to plan";
+      else
+        planText = plan.text;
+
+      if(hold.text==null || hold.text=="")
+        holdText = "Hold up";
+      else
+        holdText = hold.text;
+
+      print(urgentText + importantText + planText + holdText);
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 15,right: 15,top: 15),
+              child: TextFormField(
+                controller: urgent,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    urgent.text = value.toString();
+                  });
+                },
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Urgent",
+
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Urgent and Important',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15,right: 15,top: 15),
+              child: TextFormField(
+                controller: important,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    important.text = value.toString();
+                  });
+                },
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Important",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Important not Urgent',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: plan,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    plan.text = value.toString();
+                  });
+                },
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Need to plan",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Urgent not important',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 15,right: 15),
+              child: TextFormField(
+                controller: hold,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    hold.text = value.toString();
+                  });
+                },
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Hold up",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Not important nor Urgent',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.check),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+class DemoClass extends StatefulWidget {
+  @override
+  _DemoClassState createState() => _DemoClassState();
+}
+
+class _DemoClassState extends State<DemoClass> {
+  TextEditingController userInput = TextEditingController();
+  String text = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page Demo"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "TextFormField In Flutter",
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: userInput,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.w600,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    userInput.text = value.toString();
+                  });
+                },
+                decoration: InputDecoration(
+                  focusColor: Colors.white,
+                  //add prefix icon
+                  prefixIcon: Icon(
+                    Icons.person_outline_rounded,
+                    color: Colors.grey,
+                  ),
+
+                  errorText: "Error",
+
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                    const BorderSide(color: Colors.blue, width: 1.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  fillColor: Colors.grey,
+
+                  hintText: "Email/Mobile",
+
+                  //make hint text
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+
+                  //create lable
+                  labelText: 'Email/Mobile',
+                  //lable style
+                  labelStyle: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                    fontFamily: "verdana_regular",
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ),
+            Text(userInput.toString()),
+          ],
+        ),
+      ),
+    );
+  }
+}
